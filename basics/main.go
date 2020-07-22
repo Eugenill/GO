e3f2a5d9be5ae7f6ad255c2cbc7a9f707c6d6325
Eugeni <eugeni.llagostera@gmail.com>
2020-07-22 13:39:52 +0200

    New files and changing into go.mod

--- basics/main.go
+++ basics/main.go
@@ -1,39 +1,37 @@
 package main
 
-
-import(
-	"fmt"
+import (
 	"errors"
+	"fmt"
 	"math"
-	
 )
 
-func main(){
+func main() {
 
 	//Print Hello
 	fmt.Println("Hello there!")
-//------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------
 
 	//Sum1
 	fmt.Println("\nSum1------------------------------------------------------------------")
 	fmt.Println(sum(3, 4))
-//------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------
 
 	//Sum2
 	fmt.Println("\nSum2------------------------------------------------------------------")
 	result := sum2(3, 4)
 	fmt.Println(result)
 
-//------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------
 	//Calling a list generator
 	fmt.Println("\nList------------------------------------------------------------------")
 	list()
-//------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------
 
 	//Calling a map gen
 	fmt.Println("\nMap------------------------------------------------------------------")
-	maping()
-//------------------------------------------------------------------------------------------------------
+	fmt.Printf("%d", maping())
+	//------------------------------------------------------------------------------------------------------
 
 	//Errors
 	fmt.Println("\nErrors------------------------------------------------------------------")
@@ -43,40 +41,38 @@ func main(){
 	} else {
 		fmt.Println(result2)
 	}
-//------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------
 
 	//Struct
 	fmt.Println("\nStruct------------------------------------------------------------------")
-	p := person{name: "Eugeni", age:24}
+	p := person{name: "Eugeni", age: 24}
 	fmt.Println(p)
 	fmt.Println(p.age)
-	
-//------------------------------------------------------------------------------------------------------
+
+	//------------------------------------------------------------------------------------------------------
 	//Pointer
 	fmt.Println("\nPointer------------------------------------------------------------------")
-	i := 7 
+	i := 7
 	fmt.Println(&i, i)
-	inc(&i) //if we want to change the original number (in the same addres), 
+	inc(&i) //if we want to change the original number (in the same addres),
 	//we have to create a pointer in another variable that modifies it in a diferent function
-	fmt.Println(&i, i) 
+	fmt.Println(&i, i)
 
 }
-
 
 //------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------
 //FUNCTIONS
 
-
 //sum1
-func sum(x, y int) (result int){
+func sum(x, y int) (result int) {
 	result = x + y
 	return
 }
 
 //------------------------------------------------------------------------------------------------------
 //sum2
-func sum2(x, y int) int { 
+func sum2(x, y int) int {
 	return x + y
 }
 
@@ -86,38 +82,38 @@ func list() {
 	var a [5]int
 	a[2] = 7
 	fmt.Println(a)
-	for index, value := range a {
-		
-	}
 
-	b := [5]int{1,2,3,4,5}
+	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println(b)
 
-	c := []int{2,3,4,5}
-	c = append(c,7)
-	fmt.Println(c)	
+	c := []int{2, 3, 4, 5}
+	c = append(c, 7)
+	fmt.Println(c)
 
 }
+
 //------------------------------------------------------------------------------------------------------
 //map
-func maping(){
+func maping() int {
 	vertices := make(map[string]int) //diccionari amb key = string i values = int, the make built in function is used to create the map
-	vertices["First"]=1
-	vertices["Second"]=2
-	vertices["Third"]=3 
-	delete(vertices,"Second")
+	vertices["First"] = 1
+	vertices["Second"] = 2
+	vertices["Third"] = 3
+	delete(vertices, "Second")
 	fmt.Println(vertices)
 
 	for index, value := range vertices {
 		fmt.Println(index, vertices[index], value)
-	} 
+	}
+	return vertices[""]
 }
+
 //------------------------------------------------------------------------------------------------------
 
 //Errors
 func sqrt(x float64) (float64, error) {
 	if x < 0 {
-		return 0,  errors.New("Undefined for negative numbers")
+		return 0, errors.New("Undefined for negative numbers")
 	}
 
 	return math.Sqrt(x), nil
@@ -128,7 +124,7 @@ func sqrt(x float64) (float64, error) {
 //struct
 type person struct { //class
 	name string
-	age int
+	age  int
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -136,11 +132,7 @@ type person struct { //class
 func inc(x *int) { //we dont create any return variable
 	*x++
 }
+
 // how pointers work: https://www.golang-book.com/books/intro/8
 
-
-
 //------------------------------------------------------------------------------------------------------
-
-
-
---
+++ go.mod
@@ -0,0 +1,12 @@
+module go_examples
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.7.4
+	github.com/sqlbunny/sqlbunny v0.0.0-20200713124054-08ac697e77c5
+	golang.org/x/tour v0.0.0-20200508155540-0608babe047d
+)
